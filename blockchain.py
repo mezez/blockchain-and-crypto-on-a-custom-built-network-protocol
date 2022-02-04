2b5e021201770345bc1374b41077ef2b6a4762ba
mez <ekemammezez@gmail.com>
2022-02-04 09:05:48 +0100

    improved hashing and proof of work -mez

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,9 @@
 # initialize cheesechain list
+import json
 import sys
+import hashlib
 
-MINING_REWARD = 10 #CHEESECOIN
+MINING_REWARD = 10  # CHEESECOIN
 
 open_transactions = []
 cheesechain = []
@@ -9,15 +11,46 @@ raclette_cheese = {'previous_smell': '', 'sequence_number': 0, 'transactions': [
 cheesechain.append(raclette_cheese)
 owner = 'Mez'
 participants = {'Mez'}
+VALID_HASH_CONDITION = '00'
+
+
+def valid_proof(transactions, last_hash, proof):
+    # guess a new hash
+    guess = (str(transactions) + str(last_hash) + str(proof)).encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    print(guess_hash)
+    # check if guess hash stars with 2 leading zeros
+    return guess_hash[0:2] == VALID_HASH_CONDITION
+
+
+def proof_of_work():
+    last_cheese = cheesechain[-1]
+    last_hash = hash_cheese(last_cheese)
+    nonce = 0
+    _valid_proof = False
+    while not _valid_proof:  # execute until valid proof is true
+        _valid_proof = valid_proof(open_transactions, last_hash, nonce)
+        nonce += 1
+    return nonce
 
 
 def hash_cheese(cheese):
-    return '-'.join([str(cheese[key]) for key in cheese])  # list comprehension
+    """
+
+    :param cheese: a single unit of the cheesechain
+    :return: string hash value representing the cheese
+    """
+    return hashlib.sha256(json.dumps(cheese).encode()).hexdigest()
 
 
 def get_balance(participant):
-    #get all the transaction amount sent by user, already in the blockchain
-    tx_sender = [[tx['amount'] for tx in cheese['transactions'] if tx['sender'] == participant] for cheese in cheesechain]
+    """
+
+    :param participant: a person involved in a transaction
+    :return: a float cumulative cheesecoin balance of the participant
+    """
+    tx_sender = [[tx['amount'] for tx in cheese['transactions'] if tx['sender'] == participant] for cheese in
+                 cheesechain]
     # get all the transaction amount sent by user, waiting to be mined in open transactions queue
     open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
@@ -25,7 +58,8 @@ def get_balance(participant):
     for tx in tx_sender:
         if len(tx) > 0:
             amount_sent += tx[0]
-    tx_recipient = [[tx['amount'] for tx in cheese['transactions'] if tx['recipient'] == participant] for cheese in cheesechain]
+    tx_recipient = [[tx['amount'] for tx in cheese['transactions'] if tx['recipient'] == participant] for cheese in
+                    cheesechain]
     amount_received = 0
     for tx in tx_recipient:
         if len(tx) > 0:
@@ -34,7 +68,9 @@ def get_balance(participant):
 
 
 def get_last_cheese_chain_value():
-    """ returns the last value of the current cheesechain
+    """
+
+    :return: returns the last value of the current cheesechain
     """
     if len(cheesechain) < 1:
         return None
@@ -42,7 +78,11 @@ def get_last_cheese_chain_value():
 
 
 def verify_transaction(transaction):
-    """confirm that sender has enough balance to carry out a transaction"""
+    """
+
+    :param transaction: a dictionary containing transaction details
+    :return: confirm that sender has enough balance to carry out a transaction
+    """
     sender_balance = get_balance(transaction['sender'])
     if sender_balance >= transaction['amount']:
         return True
@@ -50,15 +90,15 @@ def verify_transaction(transaction):
 
 
 def add_transaction(recipient, sender=owner, amount=1.0):
-    """ Add a transaction
+    """
 
-    :arguments
-    sender: sender of cheesechain
-    recipient: receiver of cheesecoin
-    amount: amount of cheesecoins sent in the transaction, default value 1.0
+    :param recipient: receiver of cheesecoin
+    :param sender: sender of cheesechain
+    :param amount: amount of cheesecoins sent in the transaction, default value 1.0
+    :return: boolean
     """
     transaction = {'sender': sender, 'recipient': recipient, 'amount': amount}
-    if verify_transaction(transaction): # confirm that there is enough money in sender's account
+    if verify_transaction(transaction):  # confirm that there is enough money in sender's account
         open_transactions.append(transaction)
         participants.add(sender)
         participants.add(recipient)
@@ -69,13 +109,13 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 def mine_cheese():
     last_cheese = cheesechain[-1]  # last cheese in the chain
 
-    #method one :for loop
+    # method one :for loop
     # parent_smell = ''
     # for key in last_cheese:
     #     value = last_cheese[key]
     #     parent_smell = parent_smell + str(value)
 
-    #method two :list comprehension
+    # method two :list comprehension
     parent_smell = hash_cheese(last_cheese)
 
     reward_transaction = {
@@ -96,7 +136,10 @@ def mine_cheese():
 
 
 def get_transaction_value():
-    """ Returns the input of the user (a new transaction amount) as a float value"""
+    """
+
+    :return: Returns the input of the user (a new transaction amount) as a float value
+    """
     # random_string is used in this function scope
     try:
         transaction_recipient = input("Enter the recipient of transaction: ")
@@ -125,9 +168,10 @@ def print_cheesechain_elements():
 # proof of work
 def verify_chain():
     is_valid = True
-    for (index, cheese) in enumerate(cheesechain): # enumerate returns a tuple with the index of an element and the element itself
+    for (index, cheese) in enumerate(
+            cheesechain):  # enumerate returns a tuple with the index of an element and the element itself
         if index == 0:
-            #first element
+            # first element
             continue
         if cheese['previous_smell'] != hash_cheese(cheesechain[index - 1]):
             """comparing the previous hash of the current cheese with a recalculated hash of the cheese preceding this cheese. 
@@ -140,14 +184,13 @@ def verify_chain():
 def verify_transactions():
     return all([verify_transaction(tx) for tx in open_transactions])
 
-     # is_valid = True
+    # is_valid = True
     # for tx in open_transactions:
     #     if verify_transaction(tx):
     #         is_valid = True
     #     else:
     #         is_valid = False
     # return is_valid
-
 
 
 waiting_for_input = True
@@ -191,7 +234,7 @@ while waiting_for_input:
                     'previous_smell': '',
                     'sequence_number': 0,
                     ' transactions': [{'sender': 'Sara', 'recipient': 'Randika', 'Amount': 22.24}]
-                 }
+                }
             ]
     elif user_choice == "q":
         waiting_for_input = False
@@ -202,7 +245,7 @@ while waiting_for_input:
         print("invalid cheesechain")
         print_cheesechain_elements()
         break
-    print('Remaining Balance for {}: {:6.2f}'.format('Mez',get_balance('Mez')))
+    print('Remaining Balance for {}: {:6.2f}'.format('Mez', get_balance('Mez')))
 else:
     print("User left")
 
