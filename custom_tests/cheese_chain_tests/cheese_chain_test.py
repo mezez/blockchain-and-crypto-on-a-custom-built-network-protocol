4acb032cf5b63dfed131e2f2e653c38d8ed3b34e
Kumbar SOUMYA <ext.21m.2016@etu.emse.fr>
2022-03-18 01:57:36 +0100

    cheesechain unit tests

---
+++ custom_tests/cheese_chain_tests/cheese_chain_test.py
@@ -0,0 +1,62 @@
+import unittest
+
+class CheeseChainTest(unittest.TestCase):
+    #transaction.py
+    def test_to_ordered_dictionary(self):
+        #Pass transaction details
+        #Check if it returns ordered dictionary of transactions
+        return  True
+    #wallet.py
+    def test_create_keys(self):
+        #does not return anything
+        return True
+
+    def test_save_keys(self):
+        #Check if the wallet has public and private keys
+        #Check if the method returns True or False(boolean)
+        return True
+
+    def test_load_keys(self):
+        #
+        #Check if the value returned is boolean
+        return True
+
+    def test_generate_keys(self):
+        return True
+
+    def test_sign_transaction(self):
+        #check if it returns value
+        return True
+
+    def test_verify_signature(self):
+        return True
+
+    def test_make_string_length_even(self):
+        return True
+    #CheeseChain
+    def test_get_chain(self):
+        return True
+
+    def test_get_open_transactions(self):
+        return True
+
+    def test_load_data(self):
+        #Ask Mez
+        return True
+
+    def test_save_data(self):
+        return True
+
+    def test_overwrite_data(self):
+        return True
+
+    def test_proof_of_work(self):
+        return True
+
+    def test_get_balance(self):
+        return True
+
+    def test_get_last_cheese_chain_value(self):
+        return True
+
+
